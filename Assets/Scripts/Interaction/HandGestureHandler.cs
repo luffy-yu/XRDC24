6b137dfed3b9115772203bf573e2118e663c2e1c
Liuchuan Yu <lyu20@gmu.edu>
2025-01-03 18:22:30 -0500

    Add HandGestureHandler.cs and HandEyeTracking.unity

---
+++ Assets/Scripts/Interaction/HandGestureHandler.cs
@@ -0,0 +1,35 @@
+using Oculus.Interaction;
+using UnityEngine;
+
+namespace XRDC24.Interaction
+{
+    [RequireComponent(typeof(ActiveStateUnityEventWrapper))]
+    public class HandGestureHandler : MonoBehaviour
+    {
+        public System.Action OnGestureActivated;
+
+
+        private ActiveStateUnityEventWrapper _activeStateUnityEventWrapper;
+
+
+        void Start()
+        {
+            _activeStateUnityEventWrapper = GetComponent<ActiveStateUnityEventWrapper>();
+            _activeStateUnityEventWrapper.WhenActivated.AddListener(GestureActivated);
+        }
+
+        public void GestureActivated()
+        {
+            print("GestureActivated");
+            if (OnGestureActivated != null)
+            {
+                OnGestureActivated();
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
