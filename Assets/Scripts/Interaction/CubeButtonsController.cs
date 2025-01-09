b376f156fe5544e7c79224d2ea923f8746690ffd
Liuchuan Yu <yuliuchuan@gmail.com>
2025-01-09 15:14:59 -0500

    Add TriggerForwarder etc

---
+++ Assets/Scripts/Interaction/CubeButtonsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XRDC24.Interaction
+{
+    public class CubeButtonsController : MonoBehaviour
+    {
+        public List<TriggerForwarder> forwarders;
+
+        private void Start()
+        {
+            // bind event
+            foreach (var tf in forwarders)
+            {
+                tf.OnTriggerTriggered += OnTriggerTriggered;
+            }
+        }
+
+        private void OnTriggerTriggered(TriggerType obj)
+        {
+            switch (obj)
+            {
+                case TriggerType.Forward:
+                    OnForward();
+                    break;
+                case TriggerType.Backward:
+                    OnBackward();
+                    break;
+                case TriggerType.Recording:
+                    OnRecording();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OnForward()
+        {
+            print("OnForward");
+        }
+
+        private void OnBackward()
+        {
+            print("OnBackward");
+        }
+
+        private void OnRecording()
+        {
+            print("OnRecording");
+        }
+    }
+}
