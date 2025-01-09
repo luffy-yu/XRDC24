8c9ad6edade29be80857409b727264547e1bb2fc
Liuchuan Yu <yuliuchuan@gmail.com>
2025-01-09 12:53:04 -0500

    Add GoodbyeController

---
+++ Assets/Scripts/GestureDriven/GoodbyeController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace XRDC24.GestureDriven
+{
+    public class GoodbyeController : MonoBehaviour
+    {
+        public void SayGoodbye()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
