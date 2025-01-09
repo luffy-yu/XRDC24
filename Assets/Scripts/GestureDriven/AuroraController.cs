6e0b663cfefccbf1c93d0fec6eec79ae59b55a32
Liuchuan Yu <yuliuchuan@gmail.com>
2025-01-09 13:14:51 -0500

    Add AuroraController

---
+++ Assets/Scripts/GestureDriven/AuroraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace XRDC24.GestureDriven
+{
+    public class AuroraController : MonoBehaviour
+    {
+        public GameObject leftAurora;
+        public GameObject rightAurora;
+
+        private void Start()
+        {
+            // disable at the beginning
+            leftAurora.SetActive(false);
+            rightAurora.SetActive(false);
+        }
+
+        public void EnableLeftAurora()
+        {
+            leftAurora.SetActive(true);
+        }
+
+        public void EnableRightAurora()
+        {
+            rightAurora.SetActive(true);
+        }
+    }
+}
