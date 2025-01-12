4387b3641f4cbc78f7285077e629f06cc844d626
Liuchuan Yu <yuliuchuan@gmail.com>
2025-01-12 10:16:53 -0500

    Add SummaryController

---
+++ Assets/Scripts/Summary/SummaryController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace XRDC24.Summary
+{
+    public class SummaryController : MonoBehaviour
+    {
+        public List<TextMeshProUGUI> positives;
+        public List<TextMeshProUGUI> negatives;
+        public List<TextMeshProUGUI> durations;
+
+        public List<TextMeshProUGUI> sentences;
+
+        public int positiveCount;
+        public int negativeCount;
+        public int duration;
+
+        string GenerateSentence()
+        {
+            var s = $"" +
+                    $"You gathered {positiveCount} positive thoughts " +
+                    $"and overcame {negativeCount} negative ones, " +
+                    $"and finished {duration} minutes meditation process today.";
+            return s;
+        }
+
+        public void UpdateSummary(int pos, int neg, int duration)
+        {
+            positiveCount = pos;
+            negativeCount = neg;
+            this.duration = duration;
+
+            // Update UI
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            positives.ForEach(item => item.text = $"{positiveCount}");
+            negatives.ForEach(item => item.text = $"{negativeCount}");
+            durations.ForEach(item => item.text = $"{duration}");
+
+            var s = GenerateSentence();
+            sentences.ForEach(item => item.text = s);
+        }
+    }
+}
