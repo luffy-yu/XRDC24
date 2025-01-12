8e745b56cd198cd309fbd4a36339b62ad61cf7f4
yvettemuki <373628977@qq.com>
2025-01-11 22:01:37 -0500

    feat: add gradually disappearing effect to the font

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Constants
 {
     public readonly static int TITLE_SHOW_SECONDS = 5;
+    public readonly static float TITLE_DISAPPEAR_DURATION = 1.5f;
     public readonly static int ONBOARD_PANEL_SHOW_SECONDS = 5;
     public readonly static int SPEAK_MAX_DURATION = 30;
     public readonly static int RT_WIDTH = 1334;
