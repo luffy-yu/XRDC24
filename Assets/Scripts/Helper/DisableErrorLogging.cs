2a012d1ea9d354bdd0cfd631d28a03eb44fc5933
Liuchuan Yu <yuliuchuan@gmail.com>
2025-01-15 11:55:23 -0500

    Add DisableErrorLogging

---
+++ Assets/Scripts/Helper/DisableErrorLogging.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace XRDC24.Helper
+{
+    public class DisableErrorLogging: MonoBehaviour
+    {
+        void Awake()
+        {
+            Application.logMessageReceived += SuppressLogs;
+        }
+
+        private void SuppressLogs(string condition, string stackTrace, LogType type)
+        {
+            if (type == LogType.Error || type == LogType.Exception)
+            {
+                return;
+            }
+            
+            Debug.Log(condition);
+        }
+
+        void OnDestroy()
+        {
+            Application.logMessageReceived -= SuppressLogs;
+        }
+    }
+}
